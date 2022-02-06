500efb410ee38e4126dea0876535ff82395a97ae
bshea822 <41710509+bshea822@users.noreply.github.com>
2022-02-06 09:40:15 -0600

    Added ability to move the ship to the left.

--- alien_invasion.py
+++ alien_invasion.py
@@ -38,10 +38,16 @@ class AlienInvasion:
                 if event.key == pygame.K_RIGHT:
                     # Move ship to the right.
                     self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    # Move the ship to the left.
+                    self.ship.moving_left = True
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     # Halt the ship from moving right.
                     self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    # Halt the ship from moving left.
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
